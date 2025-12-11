1a24f503ac3690043680a1eb7af77ef6e4c3eae4
adarsh <adarshshanu3@gmail.com>
2025-12-11 08:57:06 +0530

    routes fixed

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:5000/api/ocr/extract'
+  production: true,
+  // apiUrl: 'http://localhost:5000/api/ocr/extract'
+  apiUrl: 'http://ocr.adarsh.services/api/ocr/extract',
 };
